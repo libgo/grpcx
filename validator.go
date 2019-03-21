2fec0c0280d39fb3706205233db898afbcfa7cc7
elvizlai <sdrzlyz@gmail.com>
2019-03-21 13:04:18 +0800

    add validator

---
+++ validator.go
@@ -0,0 +1,45 @@
+package grpcx
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type validator interface {
+	Validate() error
+}
+
+// Validate checks if unary incoming proto is Valid
+func Validate(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, err error) {
+	if v, ok := req.(validator); ok {
+		if err := v.Validate(); err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, err.Error())
+		}
+	}
+	return handler(ctx, req)
+}
+
+// ValidateStream checks if stream incoming proto is Valid
+func ValidateStream(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	wrapper := &recvWrapper{ss}
+	return handler(srv, wrapper)
+}
+
+type recvWrapper struct {
+	grpc.ServerStream
+}
+
+func (s *recvWrapper) RecvMsg(m interface{}) error {
+	if err := s.ServerStream.RecvMsg(m); err != nil {
+		return err
+	}
+	if v, ok := m.(validator); ok {
+		if err := v.Validate(); err != nil {
+			return status.Errorf(codes.InvalidArgument, err.Error())
+		}
+	}
+	return nil
+}
